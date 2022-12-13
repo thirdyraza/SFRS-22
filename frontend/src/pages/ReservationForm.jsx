9e88ae764596155df7d1abadf524f60a12665802
Siriuz <72621548+thirdyraza@users.noreply.github.com>
2022-12-14 02:06:35 +0800

    modified reservation form
    organization depends on user.dept
    + room depends on building/venue

--- frontend/src/pages/ReservationForm.jsx
+++ frontend/src/pages/ReservationForm.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux'
-import { createReserve, getReservation } from '../features/reserves/reserveSlice'
+import { createReserve, getReservation, getAllReserves, reset } from '../features/reserves/reserveSlice'
 import '../assets/scss/mainform.scss';
 
+// import ReservesHead from '../components/ReservesHead'
+// import ReservesContent from '../components/ReservesContent'
+
 function ReservationForm(){
+
+    const {user} = useSelector((state) => state.auth)
+    const {reserves, allReserves} = useSelector((state) => state.reserves)
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        dispatch(getAllReserves())
+        return () => {
+            dispatch(reset())
+        };
+    }, [dispatch]);
+
     const [formData, setFormData] = useState({
         activity: '',
         purpose: '',
@@ -16,11 +33,59 @@ function ReservationForm(){
         time_out: '',
     })
 
-    const { activity, purpose, org, venue, room, date, time_in, time_out } = formData
-    const {reserves} = useSelector((state) => state.reserves)
+    const SEAITE = ["PICE", "IIEE", "LITES", "UAPSA", "JIEEP", "LTL", "SSC", "CCA", "LUSC" ]
+    const SABH = ["HOST", "JFINEX", "JPIA", "JMAH", "LTL", "SSC", "CCA", "LUSC" ]
+    const SEAS = ["LIFE", "JAPS", "PSS", "CRIM", "LTL", "SSC", "CCA", "LUSC" ]
+    const SHAS = ["LPSO", "LNSO", "LMTO", "LTL", "SSC", "CCA", "LUSC" ]
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate
+    const C = ["C 21", "C 22", "C 23", "C 24", "C 25", "C 26"]
+    const B = ["B 11", "B 12", "B 13", "B 14"]
+    const K = ["K 11", "K 12", "K 13", "K 14"]
+    const N = ["N 21", "N 22", "N 23", "N 24", "N 25", "N 26"]
+    const J = ["J 41", "J 42", "J 43", "J 44", "J 45", "J 46", "J 47"]
+
+    const facis = ["Open Stage", "James Ter Mier Gymnasium", "Bulwagang Teodulfo", "Bulwagang Andres Nowe", "Friendship Park",
+        "C Building", "B Building", "N Building", "J Building", "K Building",
+        "EE Laboratory", "ECE Laboratory", "GE Laboratory"]
+    const empt = ["Not Applicable"]
+    const ee = ["K 12"]
+    const ece = ["K 11"]
+    const ge = ["B 13"]
+
+    let underDept = null
+    let orgs = null
+    let bldgs
+    let underBldg = null
+    let noRooms = null
+    let rooms = null
+
+    const { activity, purpose, org, venue, room, date, time_in, time_out } = formData
+
+    if (user.dept === 'SEAITE') {underDept = SEAITE}
+    else if (user.dept === 'SABH') { underDept = SABH }
+    else if (user.dept === 'SEAS') { underDept = SEAS }
+    else if (user.dept === 'SHAS') { underDept = SHAS }
+
+    if(underDept){ orgs = underDept.map((e) => <option key={e}>{e}</option>) }
+    bldgs = facis.map((e) => <option key={e}>{e}</option>)
+
+    if(venue === 'C Building'){ underBldg = C }
+    else if (venue === 'B Building'){ underBldg = B}
+    else if (venue === 'K Building'){ underBldg = K}
+    else if (venue === 'N Building'){ underBldg = N}
+    else if (venue === 'J Building'){ underBldg = J}
+
+    if(venue === 'Open Stage' || venue === 'James Ter Mier Gymnasium' || venue === 'Friendship Park' || venue === 'Bulwagang Teodulfo' || venue === 'Bulwagang Andres Nowe'){
+        noRooms = empt
+    }else if(venue === 'EE Laboratory') { noRooms = ee }
+    else if(venue === 'ECE Laboratory') { noRooms = ece }
+    else if(venue === 'GE Laboratory') { noRooms = ge }
+
+    if(venue === 'EE Laboratory'){}
+
+    if(underBldg){ rooms = underBldg.map((e) => <option key={e}>{e}</option>)}
+    else if(noRooms) { rooms = noRooms.map((e) => <option key={e}>{e}</option>)}
+    
 
     const onChange = (e) =>{
         setFormData((prevState) => ({
@@ -53,7 +118,7 @@ function ReservationForm(){
     return(
     <div className="app">
         <div id="home">
-        <div>
+            <div>
                 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
                 <div className="reserve-form-container">
                     <form onSubmit={onSubmit}>
@@ -99,11 +164,7 @@ function ReservationForm(){
                                     value = {org}
                                     onChange={onChange}>
                                         <option hidden>- - - - -</option>
-                                        <option>LITES</option>
-                                        <option>IIEE</option>
-                                        <option>GESA</option>
-                                        <option>UAPSA</option>
-                                        <option>JCIEPEP</option>
+                                        {orgs}
                                     </select>
                                 </div>
                                 <div className="in">
@@ -113,14 +174,10 @@ function ReservationForm(){
                                     name='venue'
                                     id='venue'
                                     value = {venue}
-                                    onChange={onChange}
+                                    onChange={onChange}                                
                                     >
                                         <option hidden>- - - - -</option>
-                                        <option>R Building</option>
-                                        <option>N Building</option>
-                                        <option>J Building</option>
-                                        <option>H Building</option>
-                                        <option>K Building</option>
+                                        {bldgs}
                                     </select>
                                 </div>
                                 <div className="in">
@@ -131,12 +188,7 @@ function ReservationForm(){
                                     id='room'
                                     value = {room}
                                     onChange={onChange}>
-                                        <option hidden>- - - - -</option>
-                                        <option>R 21</option>
-                                        <option>N 46</option>
-                                        <option>J 43</option>
-                                        <option>H 23</option>
-                                        <option>K 13</option>
+                                        {rooms}
                                     </select>
                                 </div>
                         </div>
